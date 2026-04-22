eaf5ddc69d65102795387631d25cabcc1885a054
CesaladM07 <735942@student.dusd.net>
2026-04-22 11:43:00 -0700

    Scripts and more
    Added like 3 new scripts and create any enemy prefabs. Added new physic.

---
+++ P4Unit4Mojica/Assets/Course Library/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 3.0f;
+    private Rigidbody enemyRb;
+    private GameObject player;
+
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        // Automatically finds the player by tag
+        player = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * speed);
+    }
+}
